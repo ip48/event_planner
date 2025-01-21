96d3f9442f095d033fe598f8516a41068937a56b
ip48 <innapolak@gmail.com>
2025-01-21 07:09:04 -0500

    add my footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Footer = ()=>{
+    return (
+        <>
+        <footer>
+        <p>&copy; Event Planner Organization. All rights reserved.</p>
+            <div>
+                <section>
+                    <p> first note </p>
+                    <p> second note </p>
+                </section>
+                <section>
+                    <p> first remark </p>
+                    <p> second remark </p>
+                </section>
+
+            </div>
+            <div>
+                <section>
+                    <p> first comment </p>
+                    <p> second comment </p>
+                </section>
+            </div>
+
+        </footer>
+        </>
+    )
+
+}
+
+export default Footer;
